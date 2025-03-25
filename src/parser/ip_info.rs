45b15a5303409243214152a19e3a1d799ff0ea8c
GyulyVGC <gyulyvgc99@gmail.com>
2025-03-25 16:42:54 +0100

    IP info routine: only login to datastore if token is expired

--- src/parser/ip_info.rs
+++ src/parser/ip_info.rs
@@ -1,11 +1,14 @@
 use crate::datastore::DatastoreWrapper;
 use crate::utils::{ACCOUNT_ID, ACCOUNT_SECRET};
 use indexmap::IndexSet;
-use nullnet_liberror::Error;
+use nullnet_liberror::{location, Error, ErrorHandler, Location};
 use nullnet_libipinfo::{ApiFields, IpInfoHandler, IpInfoProvider};
+use nullnet_libtoken::Token;
 use std::net::IpAddr;
 use std::sync::mpsc::Receiver;
+use std::sync::Arc;
 use tokio::runtime::Handle;
+use tokio::sync::Mutex;
 
 const IP_INFO_API_KEY: Option<&str> = option_env!("IP_INFO_API_KEY");
 
@@ -15,32 +18,47 @@ pub fn ip_info_handler(
     rt_handle: &Handle,
     ds: &DatastoreWrapper,
 ) {
+    let token: Arc<Mutex<Option<Token>>> = Arc::default();
     let mut ip_cache = IpCache::new(cache_size);
     for ip in rx.iter().flatten() {
         let is_cached = ip_cache.contains(ip);
         ip_cache.refresh(ip);
         if !is_cached {
             let ds = ds.clone();
+            let token = token.clone();
             rt_handle.spawn(async move {
-                get_and_store_ip_info(ip, ds).await.unwrap_or_default();
+                get_and_store_ip_info(ip, ds, token)
+                    .await
+                    .unwrap_or_default();
             });
         }
     }
 }
 
-async fn get_and_store_ip_info(ip: IpAddr, mut ds: DatastoreWrapper) -> Result<(), Error> {
+async fn get_and_store_ip_info(
+    ip: IpAddr,
+    mut ds: DatastoreWrapper,
+    token: Arc<Mutex<Option<Token>>>,
+) -> Result<(), Error> {
     let ip = ip.to_string();
-    let token = ds
-        .login(ACCOUNT_ID.to_string(), ACCOUNT_SECRET.to_string())
-        .await?;
 
-    let is_stored = ds.is_ip_info_stored(ip.as_str(), token.as_str()).await?;
+    let mut token = token.lock().await;
+    if token.as_ref().is_none_or(Token::is_expired) {
+        let new_token = ds
+            .login(ACCOUNT_ID.to_string(), ACCOUNT_SECRET.to_string())
+            .await?;
+        *token = Some(Token::from_jwt(new_token.as_str()).handle_err(location!())?);
+    }
+    let jwt = token.as_ref().unwrap().jwt.clone();
+    drop(token);
+
+    let is_stored = ds.is_ip_info_stored(ip.as_str(), &jwt).await?;
 
     if !is_stored {
         log::info!("Looking up IP information for {ip}");
         let ip_info = HANDLER.lookup(&ip).await?;
         log::info!("Looked up IP information for {ip}: {ip_info:?}");
-        ds.insert_ip_info(&ip, ip_info, &token).await?;
+        ds.insert_ip_info(&ip, ip_info, &jwt).await?;
     }
 
     Ok(())
