a15da879648b3e3ffc265fad878e024b119e6e9d
Anton Liashkevich <anton.liashkevich.eng@gmail.com>
2025-05-16 23:09:34 +0000

    Clean env

--- src/grpc_server/endpoints/mod.rs
+++
@@ -1,8 +0,0 @@
-mod common;
-mod handle_config;
-mod handle_logs;
-mod handle_packets;
-mod heartbeat;
-mod request_control_channel;
-
-pub use heartbeat::AuthHandler;
--- src/proto/mod.rs
+++
@@ -1,4 +0,0 @@
-#![allow(clippy::pedantic)]
-#[allow(clippy::enum_variant_names)]
-#[rustfmt::skip]
-pub mod wallguard;
